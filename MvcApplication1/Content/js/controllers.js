c3f23ec3506465663a4352e05fd5be49fc64cc88
Joe Moses <jmoses@acculynx.com>
2013-11-16 16:09:35 -0600

    added boostrap nav and layout and angularjs

---
+++ MvcApplication1/Content/js/controllers.js
@@ -0,0 +1,13 @@
+﻿function ListCtrl($scope) {
+    $scope.listitems = [{
+        Name: 'test1'
+    }, {
+        Name: 'test2'
+    }, {
+        Name: 'test3'
+    }];
+
+    $scope.clickListItem = function (idx) {
+        console.log(idx);
+    }
+}
