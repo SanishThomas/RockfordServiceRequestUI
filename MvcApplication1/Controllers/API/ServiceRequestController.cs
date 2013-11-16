17f2d42ebb40a08c14f05f8e206eba2578fab1c7
Sanish Thomas <sanish_vt@yahoo.com>
2013-11-16 15:24:01 -0600

    initial commit

---
+++ MvcApplication1/Controllers/API/ServiceRequestController.cs
@@ -0,0 +1,39 @@
+﻿using ServiceData.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers.API
+{
+    public class ServiceRequestController : ApiController
+    {
+
+        public HttpResponseMessage Get()
+        {
+            HttpResponseMessage response = new HttpResponseMessage();
+            List<ProblemData> ProblemDatas = new ProblemDataContext().ProblemDatas.ToList<ProblemData>();
+            response = Request.CreateResponse(HttpStatusCode.OK, ProblemDatas);
+            return response;
+        }
+
+        public HttpResponseMessage Post(ProblemData problemData)
+        {
+            HttpResponseMessage response = new HttpResponseMessage();
+            problemData.ID = Guid.NewGuid();
+            using (var dbContext = new ProblemDataContext())
+            {
+                dbContext.ProblemDatas.Add(problemData);               
+                int result = dbContext.SaveChanges();
+            }
+            response = Request.CreateResponse<ProblemData>(HttpStatusCode.OK, problemData);
+            return response;
+        }
+
+    }
+}
---
+++ MvcApplication1/Models/ProblemDataModels.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ServiceData.Models
+{
+    public class ProblemDataContext : DbContext
+    {
+        public DbSet<ProblemData> ProblemDatas { get; set; }
+    }
+
+    [Table("ProblemData")]
+    public class ProblemData
+    {
+        [Key]
+        public Guid ID { get; set; }
+        public string UserID { get; set; }
+        public decimal Latitude { get; set; }
+        public string Longitude { get; set; }
+        public string Description { get; set; }
+        public string Severity { get; set; }
+        public string Type { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
